770598c98737eaf8e2d87fc7f2a29390492c93d5
Prabir Shrestha <prshrest@microsoft.com>
2014-11-08 15:11:04 -0800

    ARM: New(options), Options and Service struct

--- resourcemanager/resourcemanager.go
+++ resourcemanager/resourcemanager.go
@@ -1,1 +1,51 @@
 package resourcemanager
+
+import (
+	"errors"
+	"net/http"
+)
+
+const (
+	defaultBasePath   = "https://management.azure.com/"
+	defaultApiVersion = "2014-04-01-preview"
+)
+
+func New(options *Options) (*Service, error) {
+	if options == nil {
+		return nil, errors.New("options is nil")
+	}
+
+	if options.Client == nil {
+		return nil, errors.New("options.Client is nil")
+	}
+
+	basePath := options.BasePath
+	if basePath == "" {
+		basePath = defaultBasePath
+	}
+
+	apiVersion := options.ApiVersion
+	if apiVersion == "" {
+		apiVersion = defaultApiVersion
+	}
+
+	s := &Service{
+		client:     options.Client,
+		BasePath:   basePath,
+		ApiVersion: apiVersion,
+	}
+
+	return s, nil
+}
+
+type Options struct {
+	Client     *http.Client
+	BasePath   string
+	ApiVersion string
+}
+
+type Service struct {
+	client     *http.Client
+	BasePath   string
+	ApiVersion string
+}
